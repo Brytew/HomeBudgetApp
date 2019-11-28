84fe66929bbffebab9256633a06a18aa7c7c0d15
Brytew <brytew2@gmail.com>
2019-11-28 14:41:50 +0100

    Added a files from IntelliJ

---
+++ src/main/java/com/homebudget/homeentirety/HomeentiretyApplication.java
@@ -0,0 +1,13 @@
+package com.homebudget.homeentirety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeentiretyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeentiretyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homebudget/homeentirety/HomeentiretyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homebudget.homeentirety;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeentiretyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
