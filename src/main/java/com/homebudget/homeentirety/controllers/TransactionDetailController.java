fa3c8dd5ff168a1a08d26f644791cd0ae0a58a10
Brytew <brytew2@gmail.com>
2020-04-22 21:00:42 +0200

    Template: summing and showing rest of budget
    From endpoint GET - we give a list of total transactions, fully expensions and rest from a budget.

--- src/main/java/com/homebudget/homeentirety/controllers/TransactionDetailController.java
+++ src/main/java/com/homebudget/homeentirety/controllers/TransactionDetailController.java
@@ -4,9 +4,11 @@ import com.homebudget.homeentirety.models.TransactionDetail;
 import com.homebudget.homeentirety.repositories.TransactionDetailRepository;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.*;
 
 @Data
@@ -27,12 +29,29 @@ public class TransactionDetailController {
         return transactionDetailRepository.findAllByTransactionSummaryId(transactionSummaryId);
     }
 
-
     @GetMapping("/current")
-    public String getFullMonthlyExpenses() {
+    public List<String> getFullMonthlyExpenses() {
 
         List<TransactionDetail> listOfMonthyTransactions = transactionDetailRepository.findAll();
-        Map<Long, BigDecimal> mapOfTransactionDetails = new HashMap<>();
+        Map<Long, BigDecimal> mapOfTransactionDetails = new TreeMap<>();
+
+        mapOfTransactionDetails.put(1L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(2L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(3L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(4L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(5L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(6L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(7L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(8L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(9L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(10L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(11L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(12L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(13L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(14L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(15L, BigDecimal.ZERO);
+        mapOfTransactionDetails.put(16L, BigDecimal.ZERO);
+
 
         for (TransactionDetail loopOfTransactionDetails : listOfMonthyTransactions) {
             BigDecimal value = mapOfTransactionDetails.get(loopOfTransactionDetails.getTransactionSummaryId());
@@ -43,16 +62,29 @@ public class TransactionDetailController {
             }
         }
 
-        String string = (mapOfTransactionDetails.toString());
 
-        return string;
+        BigDecimal summedUpExpension = new BigDecimal(0);
+        for (Map.Entry<Long, BigDecimal> entry : mapOfTransactionDetails.entrySet()) {
+            summedUpExpension = summedUpExpension.add(entry.getValue());
+        }
+
+        BigDecimal fullOfMonthlyBudget = new BigDecimal(1340);
+        BigDecimal restOfMonthlyBudget = fullOfMonthlyBudget.subtract(summedUpExpension);
 
 
-        //        ListIterator<TransactionDetail> iter = abc.listIterator();
-//        while (iter.hasNext()) {
-//            aa.add(iter.next());
-//            System.out.println("wartość : " + iter.next());
-//        }
+        List<String> templateListOfExpenses = new ArrayList<>();
+        for (Map.Entry<Long, BigDecimal> entryString : mapOfTransactionDetails.entrySet()) {
+            String templateStringToViewExpenses = entryString.toString();
+            templateListOfExpenses.add(templateStringToViewExpenses);
+        }
 
+        templateListOfExpenses.add("------");
+        templateListOfExpenses.add(summedUpExpension.toString());
+        String ileZostalo = ("(1340) = " + restOfMonthlyBudget.toString());
+        templateListOfExpenses.add(ileZostalo);
+
+        return templateListOfExpenses;
     }
 }
+
+
